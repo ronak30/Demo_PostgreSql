22f8cc062af3e278b90a69929e8765ec3cda7054
Ronak <ronak@promact.com>
2016-07-14 18:23:43 +0530

    First Demo of PostgreSQL

---
+++ src/Demo_PostGreSql/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_PostGreSql.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Role { get; set; }
+    }
+}
